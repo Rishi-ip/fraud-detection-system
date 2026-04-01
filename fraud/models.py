bb1a6b094b0cb4b8a44e680b0427e3137a2b36ac
Rishi-ip <rcp777993@gmail.com>
2026-04-01 20:53:04 +0530

    feat: create Transaction model for MongoDB

--- fraud/models.py
+++ fraud/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import mongoengine as me
+from datetime import datetime
 
-# Create your models here.
+class Transaction(me.Document):
+    amount = me.FloatField(required=True)
+    time = me.FloatField(required=True)
+    
+    features = me.ListField(me.FloatField())
+    
+    is_fraud = me.BooleanField(default=False)
+    confidence = me.FloatField(default=0.0)
+    top_features = me.ListField()
+    
+    created_at = me.DateTimeField(default=datetime.utcnow)
+    
+    meta = {
+        'collection': 'transactions',
+        'ordering': ['-created_at']
+    }
+    
+    def __str__(self):
+        status = "FRAUD" if self.is_fraud else "SAFE"
+        return f"Transaction {status} - Amount: {self.amount}"
